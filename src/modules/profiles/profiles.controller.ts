306b6e68b3881e4f8b434e279e8f7488629e26c7
Hammad Umar <hammadhawc227@gmail.com>
2026-04-04 12:53:10 +0500

    response serialization using zod get profile example

--- src/modules/profiles/profiles.controller.ts
+++ src/modules/profiles/profiles.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   Controller,
   Get,
+  HttpStatus,
   Param,
   ParseIntPipe,
   Post,
@@ -9,6 +10,8 @@ import {
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { ProfilesService } from './profiles.service';
+import { ZodResponse } from 'nestjs-zod';
+import { ProfileResponseDto } from './dto/profile-response.dto';
 
 @ApiTags('Profile')
 @Controller('profiles')
@@ -25,6 +28,7 @@ export class ProfilesController {
   }
 
   @Get(':userId')
+  @ZodResponse({ type: ProfileResponseDto, status: HttpStatus.OK })
   @ApiOperation({ summary: 'Find user profile by user ID' })
   async findOne(@Param('userId', ParseIntPipe) userId: number) {
     return this.profilesService.findOne(userId);
